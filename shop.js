ee50aae539f2c2b25685209ffc17a4bc027b04b1
Rein van Rossum <rein.vanrossum@zwijsen.nl>
2025-06-25 10:43:58 +0200

    retrigger

--- shop.js
+++ shop.js
@@ -14,6 +14,7 @@ function addToBasket(product) {
   const basket = getBasket();
   basket.push(product);
   localStorage.setItem("basket", JSON.stringify(basket));
+  console.log(basket);
 }
 
 function clearBasket() {
